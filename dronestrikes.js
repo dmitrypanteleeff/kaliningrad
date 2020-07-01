cbe6aaaedc87a31f121a098d7b1ec0821ac3b2db
Dmitry <DmitryPanteleeff@yandex.ru>
2020-07-01 23:09:11 +0300

    Сделал карту и нанёс несколько меток

---
+++ dronestrikes.js
@@ -0,0 +1,95 @@
+var dronestrikes = { 
+    "type": "FeatureCollection", "features": [
+         { "type":"Feature",
+          "geometry": {
+              "type": "Point",         
+               "coordinates":[30.70,59.95]
+               },
+               "properties": {
+                "code":"LNN",
+                "GeogLat":59.95,
+                "GeogLon":30.70,
+                "RusName":"Воейково (Ленинград)",
+                "EngName":"Voyekovo (Leningrad)",
+                "alt":"1300 м",
+                "open1":"1963",
+                "country":"Россия",
+                "intmag":"С 2005 г.",
+                "inst":"ДТОО «Институт ионосферы» АО «НЦКИТ» http://www.ionos.kz/"
+               }
+           },
+      {"type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [151.02, 60.12]
+      },
+      "properties": {
+                "code":"MGD",
+                "GeogLat":60.12,
+                "GeogLon":151.02,
+                "RusName":"Магадан",
+                "EngName":"Magadan",
+                "alt":" ",
+                "open1":"1957",
+                "country":"Россия",
+                "intmag":"С 2004 г.",
+                "inst":"Институт космофизических исследований и распространения радиоволн ДВО РАН  http://www.ikir.ru/ru/About"
+      }
+     },
+     { "type":"Feature",
+          "geometry": {
+              "type": "Point",         
+               "coordinates":[37.31,55.47]
+               },
+               "properties": {
+                "code":"MOS",
+                "GeogLat":55.47,
+                "GeogLon":37.31,
+                "RusName":"Москва",
+                "EngName":"Moscow",
+                "alt":"190 м",
+                "open1":"1919",
+                "country":"Россия",
+                "intmag":"С 2011 г.",
+                "inst":"Институт земного магнетизма и распространения радиоволн РАН  им. Н.В.Пушкова http://www.izmiran/ru/"
+               }
+           },
+    
+     { "type":"Feature",
+          "geometry": {
+              "type": "Point",         
+               "coordinates":[82.90,55.03]
+               },
+               "properties": {
+                "code":"NVS",
+                "GeogLat":55.03,
+                "GeogLon":82.90,
+                "RusName":"Новосибирск",
+                "EngName":"Novosibirsk",
+                "alt":"1020 м",
+                "open1":"1957",
+                "country":"Украина",
+                "intmag":"С 2004 г.",
+                "inst":"Львовский центр Института космических исследований  НАНУ НКАУ http://www.isr.lviv.ua/index_ru.htm"
+               }
+           },
+     { "type":"Feature",
+          "geometry": {
+              "type": "Point",         
+               "coordinates":[158.63,53.10]
+               },
+               "properties": {
+                "code":"PET",
+                "GeogLat":53.10,
+                "GeogLon":158.63,
+                "RusName":"Паратунка (Петропавловск)",
+                "EngName":"Paratunka (Petropavlovsk)",
+                "alt":" ",
+                "open1":"1967",
+                "country":"Россия",
+                "intmag":"С 2003 г.",
+                "inst":"Институт космофизических исследований и распространения радиоволн ДВО РАН http://www.ikir.ru/ru/About"
+               }
+           }
+     ] 
+}
