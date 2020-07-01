ee1d89bc212f4dac0c899187f0c753cd8fd37dd7
Dmitry <DmitryPanteleeff@yandex.ru>
2020-07-02 02:14:19 +0300

    экспериментирую с json
    пока оставил одинарные метки

---
+++ places.js
@@ -0,0 +1,57 @@
+var places = { 
+ "type": "FeatureCollection", "features":[  
+    { "type":"Feature",
+     "geometry":{          "type":"Point",
+         "coordinates":[54.697851,20.509628],
+          "properties": {               "id":"1",
+               "name":"���� ���⮣� ᥬ���⢠",
+               "adress":"�. ������� ����쭨檮��, 61�"
+              }
+      } }
+ ,
+  
+     "geometry":{          "type":"Point",
+         "coordinates":[54.70391,20.515689],
+          "properties": {               "id":"2",
+               "name":"����⠫ �롭�� ��ॢ��",
+               "adress":"�. ������᪠� 㫨�, 6"
+              }
+      } }
+ ,
+  { "type":"Feature",
+     "geometry":{          "type":"Point",
+         "coordinates":[54.695403,20.521671],
+          "properties": {               "id":"3",
+               "name":"�ਤ����᪨� ����",
+               "adress":"�. ���ন�᪮��, 30"
+              }
+      } }
+ ,
+  { "type":"Feature",
+     "geometry":{          "type":"Point",
+         "coordinates":[54.70508,20.507244],
+          "properties": {               "id":"4",
+               "name":"��������ࠤ᪨� �����⭮� �㧥� ����ࠧ�⥫��� ������",
+               "adress":"�����᪨� ���., 83"
+              }
+      } }
+ ,
+  { "type":"Feature",
+     "geometry":{          "type":"Point",
+         "coordinates":[54.714756,20.459163],
+          "properties": {               "id":"5",
+               "name":"107 ��⮡�� � ���⨩�",
+               "adress":"�. �ਤ�� �������, 1"
+              }
+      } }
+ ,
+  { "type":"Feature",
+     "geometry":{          "type":"Point",
+         "coordinates":[54.733984,20.4848],
+          "properties": {               "id":"6",
+               "name":"120 ��⮡�� � �����",
+               "adress":"�����᪨� ��ᯥ��, 84-90"
+              }
+      } 
+ ]
+}
